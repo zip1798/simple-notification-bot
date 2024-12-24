61fb58ede528e42112391e2dc6dd4e86a8767697
Oleksandr Tsivun <067x5016783@gmail.com>
2024-12-24 23:37:39 +0200

    change path to env file

--- send.js
+++ send.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
-require('dotenv').config();
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '.env') });
 
 // Отримання токена з .env файлу
 const BOT_TOKEN = process.env.BOT_TOKEN;
